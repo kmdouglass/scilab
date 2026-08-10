5dff595a37789b4f0af96561f35586d40ae4eb69
Kyle Douglass <kyle.douglass@epfl.ch>
2026-08-10 13:28:01 +0200

    Use cache.nixos-cuda.org for caching instead

--- servers/lebpc39/configuration.nix
+++ servers/lebpc39/configuration.nix
@@ -9,11 +9,12 @@ in {
     ./mqtt.nix
   ];
 
-  # Nix Flakes and cached CUDA binaries
+  # Nix Flakes and CUDA cache
+  # https://nixos.wiki/wiki/CUDA
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [ "https://cuda-maintainers.cachix.org" ];
-    trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
   };
 
   # Enable nix-ld for remote VS Code development
