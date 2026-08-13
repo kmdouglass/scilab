1fb938707d12fa0c9f0271390561f38af53a04bc
Kyle Douglass <kyle.douglass@epfl.ch>
2026-08-13 08:48:00 +0200

    Run the focus field viewer as a service

--- servers/lebpc39/flake.nix
+++ servers/lebpc39/flake.nix
@@ -9,14 +9,26 @@
     # see README.md for how to set it up.
     scilab-private.url = "git+file:///home/kmd/src/scilab-private";
     scilab-private.flake = false;
+
+    focus-field-viewer.url = "github:LEB-EPFL/focus-field-viewer";
   };
 
-  outputs = { self, nixpkgs, scilab-private, ... }@inputs: {
+  outputs = { self, nixpkgs, scilab-private, focus-field-viewer, ... }@inputs: {
     nixosConfigurations.lebpc39 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit scilab-private; };
       modules = [
         ./configuration.nix
+
+        focus-field-viewer.nixosModules.default
+        {
+          services.focus-field-viewer = {
+            enable = true;
+            port = 8501;          # default
+            address = "0.0.0.0";  # default; binds to all interfaces for LAN access
+            openFirewall = true;  # opens `port` in the NixOS firewall
+          };
+        }
       ];
     };
   };
